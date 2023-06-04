e41aa7dd90e72913ef1952c1366ecff161138b17
DianaPyanova <dpyanova-22@edu.ranepa.ru>
2023-06-04 21:48:06 +0300

    task-25

---
+++ task-25.py
@@ -0,0 +1,10 @@
+def check_number(number: int) -> None:
+    for i in range(2, round(number**0.5) + 1):
+        if number % i == 0:
+            print('Число не является простым')
+            return
+    print('Число является простым')
+
+
+num = int(input())
+check_number(num)
