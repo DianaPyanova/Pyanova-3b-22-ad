0213128626bd49edccf5bf26a5d0a2c620c4e979
DianaPyanova <dpyanova-22@edu.ranepa.ru>
2023-06-25 00:34:30 +0300

    calculator

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def addition(a, b):
+    return a + b
+def substraction (a, b):
+    return a - b
+def multiplication(a, b):
+    return a * b
+def devision(a, b):
+    if b != 0:
+        return a / b
+    else:
+        print('Ошибка: делить на 0 нельзя')
