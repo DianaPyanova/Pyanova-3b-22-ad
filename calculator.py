6c9d44fc024ee340b97f45d13bb8f8bf2e5b2835
DianaPyanova <dpyanova-22@edu.ranepa.ru>
2023-06-25 19:40:19 +0300

    fix(task-31)!: Fixed calculator.py & task-31.py
    Converted divided functions into one class with name Calculator, imported this class into task-31 and use his  methods with implementing methods

--- calculator.py
+++ calculator.py
@@ -1,11 +1,19 @@
-def addition(a, b):
-    return a + b
-def subtraction (a, b):
-    return a - b
-def multiplication(a, b):
-    return a * b
-def division(a, b):
-    if b != 0:
-        return a / b
-    else:
-        print('Ошибка: делить на 0 нельзя')
+class Calculator:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def addition(a, b):
+        return a + b
+
+    def subtraction(a, b):
+        return a - b
+
+    def multiplication(a, b):
+        return a * b
+
+    def division(a, b):
+        if b != 0:
+            return a / b
+        else:
+            print('Ошибка: делить на 0 нельзя')
--- task-31.py
+++ task-31.py
@@ -1,11 +1,11 @@
-import calculator
+from calculator import Calculator
 
 a = int(input('Введите первое число: '))
 b = int(input('Введите второе число: '))
 
-result_addition = calculator.addition(a, b)
-result_subtraction = calculator.subtraction(a, b)
-result_multiplication = calculator.multiplication(a, b)
-result_division = calculator.division(a, b)
+result_addition = Calculator.addition(a, b)
+result_subtraction = Calculator.subtraction(a, b)
+result_multiplication = Calculator.multiplication(a, b)
+result_division = Calculator.division(a, b)
 
 print(f'Сумма: {result_addition}, разность: {result_subtraction}, умножение: {result_multiplication}, деление: {result_division}')
